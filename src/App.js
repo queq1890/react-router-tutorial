4545233c2bd754c5c300cf7c886f09049a1c971e
queq1890 <32263803+queq1890@users.noreply.github.com>
2018-09-04 16:57:17 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,114 @@
+import React, { Component } from "react";
+import { BrowseRouter, Route, Link } from "react-router-dom";
+import BrowserRouter from "react-router-dom/BrowserRouter";
+
+//変数類の定義
+const FRIENDS = [
+  {
+    id: "serval",
+    nameJa: "サーバル",
+    nameEn: "Serval Cat",
+    family: "ネコ目ネコ科ネコ属"
+  },
+  {
+    id: "raccoon",
+    nameJa: "アライグマ",
+    nameEn: "Common racco0n",
+    family: "ネコ目アライグマ科アライグマ属"
+  },
+  {
+    id: "fennec",
+    nameJa: "フェネック",
+    nameEn: "Fennec",
+    family: "ネコ目イヌ科フェネック属"
+  }
+];
+
+const friendById = id => FRIENDS.find(friend => friend.id === id);
+
+//コンポーネントの定義
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/friends">Friends</Link>
+        </li>
+      </ul>
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/friends" component={Friends} />
+    </div>
+  </BrowserRouter>
+);
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <p>Welcome ようこそ</p>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+    <p>フレンズに投票するページです</p>
+  </div>
+);
+
+const Friends = () => (
+  <div>
+    <h2>Friends</h2>
+    <Route exact path="/friends" component={FriendList} />
+    <Route path="/friends/:id" component={Friend} />
+  </div>
+);
+
+const FriendList = () => (
+  <div>
+    {FRIENDS.map(friend => (
+      <li key={friend.id}>
+        <Link to={`/friends/${friend.id}`}>{friend.nameJa}</Link>
+      </li>
+    ))}
+  </div>
+);
+
+const Friend = props => {
+  const { id } = props.match.params;
+  const friend = friendById(id);
+
+  if (typeof friend === "undefined") {
+    return (
+      <div>
+        <p>Friends with id '{id}' does not exist.</p>
+      </div>
+    );
+  }
+
+  const containerStyle = {
+    border: "1px gray solid",
+    display: "inline-block",
+    padding: 10
+  };
+  const contentsStyle = { margin: 0 };
+
+  return (
+    <div>
+      <div style={containerStyle}>
+        <p style={contentsStyle}>{friend.family}</p>
+        <h1 style={contentsStyle}>{friend.nameJa}</h1>
+        <p style={contentsStyle}>{friend.nameEn}</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
